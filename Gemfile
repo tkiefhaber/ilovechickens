8fd89d248c404cf33a43b0296e1157dd5327c2df
Tom Kiefhaber <tom.kiefhaber@hungrymachine.com>
2013-08-17 16:01:14 -0400

    ilovemayo

--- Gemfile
+++ Gemfile
@@ -1,11 +1,3 @@
 source 'https://rubygems.org'
 gem 'sinatra'
-gem 'slim'
-gem 'sass'
-gem 'bourbon'
-gem 'dm-core'
-gem 'dm-migrations'
 gem 'thin'
-gem 'pg', :group => :production
-gem 'dm-postgres-adapter', :group => :production
-gem 'dm-sqlite-adapter', :group => :development
--- main.rb
+++ main.rb
@@ -1,47 +1,6 @@
 require 'sinatra'
-require 'slim'
-require 'sass'
-require 'bourbon'
-require './milkpunch'
-
-configure :development do
-  DataMapper.setup(:default, "sqlite3://#{Dir.pwd}/development.db")
-end
-
-configure :production do
-  DataMapper.setup(:default, ENV['DATABASE_URL'])
-end
 
 get '/' do
-  @left_punch = left_punch
-  @right_punch = right_punch
-  @bottle_punch = bottle_punch
-  slim :milkpunch
+  redirect '/index.html'
 end
 
-post '/' do
-  Milkpunch.create(:boob => params[:boob], :milkpunch => params[:milkpunch])
-  redirect to('/')
-end
-
-get('/styles.css'){ scss :styles }
-
-helpers do
-  def css(*stylesheets)
-    stylesheets.map do |stylesheet|
-      "<link href=\"/#{stylesheet}.css\" media=\"screen, projection\" rel=\"stylesheet\" />"
-    end.join
-  end
-
-  def left_punch
-    Milkpunch.last(:boob => 'left')
-  end
-
-  def right_punch
-    Milkpunch.last(:boob => 'right')
-  end
-
-  def bottle_punch
-    Milkpunch.last(:boob => 'bottle')
-  end
-end
